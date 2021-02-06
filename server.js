220b12a5e3a7b43824ccf340ba95afb3abdbf1cf
Shannon Quinn <shannon.quinn91@gmail.com>
2021-02-06 14:35:19 -0500

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", 
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+);
 
 //Routes
 require("./routes/html-routes")(app);
